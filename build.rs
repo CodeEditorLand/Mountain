554cb1f208bbb6131e86d7394f69c7246a89b7f5
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-24 23:11:00 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- build.rs
+++ build.rs
@@ -1,3 +1,22 @@
+#![allow(non_snake_case)]
+
+use std::fs;
+
 fn main() {
-    tauri_build::build()
+	println!("cargo:rerun-if-changed=Cargo.toml");
+	println!(
+		"cargo:rustc-env=CARGO_PKG_VERSION={}",
+		fs::read_to_string("Cargo.toml")
+			.expect("Cannot Cargo.toml.")
+			.lines()
+			.find(|Line| Line.starts_with("version"))
+			.expect("Cannot Version.")
+			.split('=')
+			.nth(1)
+			.expect("Cannot nth.")
+			.trim()
+			.trim_matches('"')
+	);
+
+	tauri_build::build()
 }
