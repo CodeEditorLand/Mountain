e7472512b6183e93e9e42d173675773c619c95eb
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-28 09:45:12 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Source/Library.rs
+++ Source/Library.rs
@@ -1,15 +1,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![allow(non_snake_case)]
 
+#[allow(dead_code)]
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
+fn main() {
 	tauri::Builder::default()
 		.plugin(tauri_plugin_shell::init())
 		.run(tauri::generate_context!())
 		.expect("Cannot Library.");
-}
-
-#[allow(dead_code)]
-fn main() {
-	run();
 }
