dd77b696f6674fe158f2e925eee0d449f2c9b858
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-28 09:28:01 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Source/Library.rs
+++ Source/Library.rs
@@ -1,18 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![allow(non_snake_case)]
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-	format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
 	tauri::Builder::default()
 		.plugin(tauri_plugin_shell::init())
-		.invoke_handler(tauri::generate_handler![greet])
 		.run(tauri::generate_context!())
-		.expect("error while running tauri application");
+		.expect("Cannot Library.");
 }
 
 #[allow(dead_code)]
