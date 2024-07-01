23c51f2f866bbd1b7efb2fc7768e6c1a4f4d0df1
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-01 21:09:11 +0300

    squash!

--- build.rs
+++ build.rs
@@ -15,25 +15,28 @@ struct Package {
 }
 
 fn main() {
-	println!("cargo:rerun-if-changed=Cargo.toml");
+	if !tauri_build::is_dev() {
+		println!("cargo:rerun-if-changed=Cargo.toml");
+		println!("cargo:rerun-if-changed=tauri.conf.json");
 
-	let Cargo: Toml =
-		toml::from_str(&fs::read_to_string("Cargo.toml").expect("Cannot Cargo.toml."))
-			.expect("Cannot toml.");
+		let Cargo: Toml =
+			toml::from_str(&fs::read_to_string("Cargo.toml").expect("Cannot Cargo.toml."))
+				.expect("Cannot toml.");
 
-	let Version = Cargo.package.version;
+		let Version = Cargo.package.version;
 
-	let mut Tauri: Value = serde_json::from_str(
-		&fs::read_to_string("tauri.conf.json").expect("Cannot tauri.conf.json."),
-	)
-	.expect("Cannot JSON.");
+		let mut Tauri: Value = serde_json::from_str(
+			&fs::read_to_string("tauri.conf.json").expect("Cannot tauri.conf.json."),
+		)
+		.expect("Cannot JSON.");
 
-	Tauri.get_mut("version").map(|Entry| *Entry = Value::String(Version.clone()));
+		Tauri.get_mut("version").map(|Entry| *Entry = Value::String(Version.clone()));
 
-	fs::write("tauri.conf.json", serde_json::to_string_pretty(&Tauri).expect("Cannot JSON."))
-		.expect("Cannot tauri.conf.json.");
+		fs::write("tauri.conf.json", serde_json::to_string_pretty(&Tauri).expect("Cannot JSON."))
+			.expect("Cannot tauri.conf.json.");
 
-	println!("cargo:rustc-env=CARGO_PKG_VERSION={}", Version);
+		println!("cargo:rustc-env=CARGO_PKG_VERSION={}", Version);
+	}
 
 	tauri_build::build();
 }
