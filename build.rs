133cc1b281da69c54698d656e8ca2eedd993375a
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-01 19:38:56 +0300

    squash!

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 #![allow(non_snake_case)]
 
 use serde::Deserialize;
+use serde_json::Value;
 use std::fs;
 
 #[derive(Deserialize)]
@@ -17,17 +18,23 @@ fn main() {
 	println!("cargo:rerun-if-changed=Cargo.toml");
 	println!("cargo:rerun-if-changed=tauri.conf.json");
 
-	let Version: Toml =
+	let Cargo: Toml =
 		toml::from_str(&fs::read_to_string("Cargo.toml").expect("Cannot Cargo.toml."))
 			.expect("Cannot toml.");
 
-	println!("{}", Version.package.version);
+	let Version = Cargo.package.version;
+
+	let mut Tauri: Value = serde_json::from_str(
+		&fs::read_to_string("tauri.conf.json").expect("Cannot tauri.conf.json."),
+	)
+	.expect("Cannot JSON.");
+
+	Tauri.get_mut("version").map(|Entry| *Entry = Value::String(Version.clone()));
+
+	fs::write("tauri.conf.json", serde_json::to_string_pretty(&Tauri).expect("Cannot JSON."))
+		.expect("Cannot tauri.conf.json.");
+
+	println!("cargo:rustc-env=CARGO_PKG_VERSION={}", Version);
 
 	tauri_build::build();
-
-	// println!("cargo:rustc-env=CARGO_PKG_VERSION={}", Version);
-
-	println!("WTF");
-
-	// tauri_build::build()
 }
