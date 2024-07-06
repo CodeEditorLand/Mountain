f98e4beae0b2632e8c04121d7b8eeaa394ff2d0a
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-06 17:52:13 +0300

    squash!

--- Source/Fn/Tauri.rs
+++ Source/Fn/Tauri.rs
@@ -1,6 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// @TODO: Finish this and import proper common libs from echo
 use Echo::Fn::Job::{Action, ActionResult, Fn as Job, Work, Worker, Yell::Fn as Yell};
 
 use async_trait::async_trait;
@@ -65,55 +64,57 @@ async fn Get(Path: String, Work: tauri::State<'_, Arc<Work>>) -> Result<(), Stri
 
 #[allow(dead_code)]
 pub async fn Fn() {
-	let Time = tokio::runtime::Runtime::new().expect("Cannot Runtime.");
+	tokio::runtime::Builder::new_multi_thread()
+		.enable_all()
+		.build()
+		.expect("Cannot new_multi_thread.")
+		.block_on(async {
+			let Order = Arc::new(tokio::sync::Mutex::new(
+				tokio_tungstenite::connect_async("ws://localhost:9999")
+					.await
+					.expect("Cannot connect_async.")
+					.0,
+			));
 
-	Time.block_on(async {
-		let Order = Arc::new(tokio::sync::Mutex::new(
-			tokio_tungstenite::connect_async("ws://localhost:9999")
-				.await
-				.expect("Cannot connect_async.")
-				.0,
-		));
+			let Work = Arc::new(Work::Begin());
+			let (Approval, mut Receipt) = mpsc::channel(100);
 
-		let Work = Arc::new(Work::Begin());
-		let (Approval, mut Receipt) = mpsc::channel(100);
+			// @TODO: Auto-calc number of workers
+			let Force: Vec<_> = (0..4)
+				.map(|_| {
+					tokio::spawn(Job(
+						Arc::new(Site { Order: Order.clone() }) as Arc<dyn Worker>,
+						Work.clone(),
+						Approval.clone(),
+					))
+				})
+				.collect();
 
-		// @TODO: Auto-calc number of workers
-		let Force: Vec<_> = (0..4)
-			.map(|_| {
-				tokio::spawn(Job(
-					Arc::new(Site { Order: Order.clone() }) as Arc<dyn Worker>,
-					Work.clone(),
-					Approval.clone(),
-				))
-			})
-			.collect();
+			let Builder = tauri::Builder::default();
 
-		let Builder = tauri::Builder::default();
+			// @TODO: FIX THIS
+			// #[cfg(debug_assertions)]
+			// Builder.plugin(tauri_plugin_devtools::init());
 
-		// @TODO: FIX THIS
-		// #[cfg(debug_assertions)]
-		// Builder.plugin(tauri_plugin_devtools::init());
+			Builder
+				.setup(|app| {
+					let Handle = app.handle().clone();
 
-		Builder
-			.setup(|app| {
-				let Handle = app.handle().clone();
+					tokio::spawn(async move {
+						while let Some(ActionResult) = Receipt.recv().await {
+							// @TODO: Rewrite the Emit to only emit to a specific webview which then talks to the others
+							Handle.emit("ActionResult", ActionResult).unwrap();
+						}
+					});
 
-				tokio::spawn(async move {
-					while let Some(ActionResult) = Receipt.recv().await {
-						// @TODO: Rewrite the Emit to only emit to a specific webview which then talks to the others
-						Handle.emit("file_operation_result", ActionResult).unwrap();
-					}
-				});
+					Ok(())
+				})
+				.manage(Work)
+				.invoke_handler(tauri::generate_handler![Put, Get])
+				.plugin(tauri_plugin_shell::init())
+				.run(tauri::generate_context!())
+				.expect("Cannot Library.");
 
-				Ok(())
-			})
-			.manage(Work)
-			.invoke_handler(tauri::generate_handler![Put, Get])
-			.plugin(tauri_plugin_shell::init())
-			.run(tauri::generate_context!())
-			.expect("Cannot Library.");
-
-		join_all(Force).await;
-	});
+			join_all(Force).await;
+		});
 }
