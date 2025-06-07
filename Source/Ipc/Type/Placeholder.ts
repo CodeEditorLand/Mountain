5c9aa44aa8a3e7aaf00228f6881e1d4313c2734e
Nikola Hristov <Nikola@PlayForm.Cloud>
2025-06-07 14:29:09 +0300

    Remove Cocoon files.
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

--- Source/Ipc/Type/Placeholder.ts
+++
@@ -1,6 +0,0 @@
-
-// This file serves as a placeholder within the Type module.
-// In a more complex gRPC implementation, this module would contain
-// generated TypeScript interfaces or classes from the .proto definitions.
-
-export const PlaceholderBoolean: boolean = true;
