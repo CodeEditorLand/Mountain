5bcdf7951f016e0ba1e9f6abc271ddb4380a0018
Nikola Hristov <Nikola@PlayForm.Cloud>
2025-06-19 22:31:50 +0300

    Delete Stale Files

--- Source/Vine/Error/mod.rs
+++
@@ -1,9 +0,0 @@
-// @module error (Vine)
-// @description Aggregates and re-exports all error types specific to the
-// Vine gRPC communication layer.
-//
-
-#![allow(non_snake_case)]
-
-mod VineError;
-pub use self::VineError::VineError;
